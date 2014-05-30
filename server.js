c852cf7e0ca640f09404534bb7e9d4c701a4e506
stupidpixels <frankozoid@gmail.com>
2014-05-30 01:00:23 -0400

    quick change to page

---
+++ server.js
@@ -0,0 +1,44 @@
+console.log("WORKED");
+
+var mongo = require('mongodb').MongoClient,
+      client   = require('socket.io').listen(8080).sockets ;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+  if (err) throw err;
+
+  client.on('connection', function(socket) {
+
+    var col = db.collection('messages'),
+      sendStatus = function(s) {
+        socket.emit('status', s);
+      } ;
+
+    col.find().limit(250).sort({_id:1}).toArray(function(err, res){
+      if (err) throw err;
+      socket.emit('output', res);
+    });
+
+    socket.on('Input', function(data) {
+
+      var name = data.name,
+            message = data.message,
+            whitespacePattern = /^\s*$/ ;
+
+      if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+        sendStatus('Name and message is required.');
+      }
+
+      else {
+        col.insert({name: name, message: message}, function() {
+
+          client.emit('output', [data]);
+
+          sendStatus({
+            message: 'Message Sent',
+            clear: true
+          });
+        }) ;
+      }
+      }) ;
+    }) ;
+  }) ;
